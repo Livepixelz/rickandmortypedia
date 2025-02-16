7b9a4d5fafe85d5112ebb46958a4baa9d96c573d
Sylvain Olengha <solengha@vodfactory.com>
2025-02-16 18:22:27 +0100

    redirect homepage to /characters

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'home',
+      redirect: '/characters',
+    },
+    {
       path: '/characters',
       name: 'characters',
       component: SearchView,
