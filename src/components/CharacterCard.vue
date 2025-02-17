12d97161da1c6d259cd61166e1bacede7ecb96ff
Sylvain Olengha <solengha@vodfactory.com>
2025-02-17 16:03:33 +0100

    ui quick fix

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -7,7 +7,7 @@ defineProps<{
 
 <template>
   <div class="flex flex-1 flex-row items-center w-full rounded-full bg-[#02b5cc] p-4 hover:-translate-y-1 active:translate-y-0 transition-all transition-duration-300" >
-    <img :src="character.image" class="border-2 border-slate-900 outline outline-4 outline-solid outline-[#b2df28] w-32 h-32 rounded-full shadow-md" :alt="`${character.name}'s picture`" />
+    <img :src="character.image" class="border-2 border-slate-900 outline outline-4 outline-solid outline-[#b2df28] w-32 h-32 rounded-full shadow-md mr-1" :alt="`${character.name}'s picture`" />
     <div class="ml-2">
       <h2 class="text-xl font-bold">{{ character.name }}</h2>
       <p class="text-sm">{{ character.species }}</p>
